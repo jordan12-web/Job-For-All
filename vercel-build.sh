e7010cc837fff402514ee860b4388aced95ccdce
Yordanos Molla <yordanosmolla12@gmail.com>
2026-06-21 17:14:45 +0300

    Step-20

--- vercel-build.sh
+++ vercel-build.sh
@@ -15,6 +15,7 @@ flutter pub get
 
 echo "==> Building Flutter Web (release)..."
 flutter build web --release \
+  --base-href "/" \
   --dart-define=SUPABASE_URL="$SUPABASE_URL" \
   --dart-define=SUPABASE_ANON_KEY="$SUPABASE_ANON_KEY"
 
